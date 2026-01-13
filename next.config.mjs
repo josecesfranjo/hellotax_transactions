10ce6df9015571b0858c9fdd97d81b5fa101c1ac
josecesfranjo <joseantonio.cesfranjo@room714.com>
2026-01-13 21:51:34 +0100

    CORS fija

--- next.config.mjs
+++ next.config.mjs
@@ -8,10 +8,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value:
-              process.env.NODE_ENV === "production"
-                ? "https://hellotax-app.vercel.app"
-                : "http://localhost:3000",
+            value: "https://hellotax-app.vercel.app",
           },
           {
             key: "Access-Control-Allow-Methods",
